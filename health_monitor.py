f37e80ff5eaefe14a55c65d2c2385927636d443b
ganesh12112 <ganesh@12.net>
2025-09-08 08:46:05 +0530

    Fix port and gunicorn

--- health_monitor.py
+++ health_monitor.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request 
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -38,7 +39,6 @@ def index():
 
     return render_template("index.html", result=result)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
